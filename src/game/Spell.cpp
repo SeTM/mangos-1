9af6cb3b0807a5f3bbab78801fdab546761212d5
KeRo99 <motrilwireless@gmail.com>
2011-04-13 23:07:48 +0200

    [K29] Crashfix, fixed crashfix in Spell::DoSpellHitOnUnit (thanks Janu and PSZ)

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1287,7 +1287,7 @@ void Spell::DoSpellHitOnUnit(Unit *unit, uint32 effectMask)
         return;
     }
 
-    if (unit->GetTypeId() == TYPEID_PLAYER)
+    if (unit->GetTypeId() == TYPEID_PLAYER && unit->IsInWorld())
     {
         ((Player*)unit)->GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_BE_SPELL_TARGET, m_spellInfo->Id);
         ((Player*)unit)->GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_BE_SPELL_TARGET2, m_spellInfo->Id);
